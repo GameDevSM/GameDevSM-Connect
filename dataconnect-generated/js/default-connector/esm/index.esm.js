ef9342ccd08aec6688d4de2a1468aed72405882c
Raul Valeanu <raullakatos23@gmail.com>
2025-07-18 00:08:54 +0300

    Initial Firebase Hosting + GitHub Actions setup

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'gamedevsmconnect',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'gamedevsmconnect',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
